49188126ccb7be76148fccff1b9d18c844734323
Anurag Patil <anurag.patil@headout.com>
2024-04-13 23:24:14 +0530

    feat: move to sveltekit + ts

---
+++ postcss.config.cjs
@@ -0,0 +1,11 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+
+const config = {
+  plugins: [//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+  //Some plugins, like tailwindcss/nesting, need to run before Tailwind, tailwindcss(), //But others, like autoprefixer, need to run after, //But others, like autoprefixer, need to run after,
+  tailwindcss(), 
+]//Some plugins, like tailwindcss/nesting, need to run before Tailwind, tailwindcss(), //But others, like autoprefixer, need to run after, autoprefixer],
+};
+
+module.exports = config;
